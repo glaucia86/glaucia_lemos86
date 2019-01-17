230b142807a1c2a1297fe76d4eef8ed363bb19b7
glaucia86 <glaucia_lemos86@hotmail.com>
2019-01-17 02:37:38 -0200

    Inclusão de arquivos de contribuição e aplicação do eslint no pacote.

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "plugins": [
+    "import"
+  ],
+  "rules": {
+    "no-console": "off",
+    "import/newline-after-import": "off",
+    "no-unused-vars": 0,
+    "max-len": 0
+  }
+};
--- bin/card.js
+++ bin/card.js
@@ -2,8 +2,8 @@
 // Usado para dizer ao Node.js que se trata de uma ferramenta do CLI
 
 // Declarando os módulos:
-const chalk = require('chalk')
-const boxen = require('boxen')
+const chalk = require('chalk');
+const boxen = require('boxen');
 
 // Definindo as opções para o ‘boxen’:
 const options = {
@@ -12,8 +12,8 @@ const options = {
   borderStyle: 'double',
   float: 'center',
   borderColor: '#ff4879',
-  backgroundColor: 'black'
-}
+  backgroundColor: 'black',
+};
 
 // Inclusão do Text e mais as definições do ‘chalk’:
 const data = {
@@ -30,21 +30,21 @@ const data = {
   labelGitHub: chalk.rgb(253, 245, 5).bold('    GitHub:'),
   labelLinkedIn: chalk.rgb(0, 255, 255).bold('  LinkedIn:'),
   labelWeb: chalk.rgb(191, 255, 0).bold('       Web:'),
-  labelCard: chalk.rgb(240, 13, 240).bold('      Card:')
-}
+  labelCard: chalk.rgb(240, 13, 240).bold('      Card:'),
+};
 
 // Aqui será a saída do nosso Cartão Pessoal em NPX:
-const newline = '\n'
-const heading = `${data.name} ${data.handle}`
-const working = `${data.labelWork}  ${data.work}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
-const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
-const carding = `${data.labelCard}  ${data.npx}`
+const newline = '\n';
+const heading = `${data.name} ${data.handle}`;
+const working = `${data.labelWork}  ${data.work}`;
+const twittering = `${data.labelTwitter}  ${data.twitter}`;
+const githubing = `${data.labelGitHub}  ${data.github}`;
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`;
+const webing = `${data.labelWeb}  ${data.web}`;
+const carding = `${data.labelCard}  ${data.npx}`;
 
-// Aqui devemos colocar toda a nossa saída numa única variável para que possamos usar 
-// o ‘boxen de maneira efetiva: 
-const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline + carding
+// Aqui devemos colocar toda a nossa saída numa única variável para que possamos usar
+// o ‘boxen de maneira efetiva:
+const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline + carding;
 
-console.log(chalk.green(boxen(output, options)))
+console.log(chalk.green(boxen(output, options)));
